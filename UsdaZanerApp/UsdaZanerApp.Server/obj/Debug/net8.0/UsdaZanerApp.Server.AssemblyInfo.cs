f7e81320e04ba26f8947e480eaeaed096180183f
Douglas Rosenberg <doug.rosenberg@gmail.com>
2024-04-16 11:03:46 -0500

    working on new connection string

--- UsdaZanerApp/UsdaZanerApp.Server/obj/Debug/net8.0/UsdaZanerApp.Server.AssemblyInfo.cs
+++ UsdaZanerApp/UsdaZanerApp.Server/obj/Debug/net8.0/UsdaZanerApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsdaZanerApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75bc99d0b772a3aa34b02714367d00a8e369b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bdaccdae04f4c2919c88b6255ca18ded9ea5179")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsdaZanerApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsdaZanerApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UsdaZanerApp/usdazanerapp.client/src/app/features/usda-data/services/usda-data.service.ts
+++ UsdaZanerApp/usdazanerapp.client/src/app/features/usda-data/services/usda-data.service.ts
@@ -13,6 +13,8 @@ export class UsdaDataService {
   constructor(private http: HttpClient) {}
 
   getUsdaData(): Observable<UsdaInfo[]> {
-    return this.http.get<UsdaInfo[]>(`${environment.apiBaseUrl}/api/UsdaInfo`);
+    // return this.http.get<UsdaInfo[]>(`${environment.apiBaseUrl}/api/UsdaInfo`);
+    return this.http.get<UsdaInfo[]>(`https://quickstats.nass.usda.gov/api/api_GET/?key=C2ADF26B-BD8D-328A-968F-2F175A287144&&statisticcat_desc=AREA PLANTED&unit_desc=ACRES&year__GE=2020/api/UsdaInfo`);
+
   }
 }
